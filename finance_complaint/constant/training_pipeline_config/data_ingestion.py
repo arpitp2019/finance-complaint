7af30a718406ea6f411aad357686a3fb8977c97b
arpitp2019 <arpitchamp1996@gmail.com>
2023-01-04 16:59:16 +0530

    first commit

---
+++ finance_complaint/constant/training_pipeline_config/data_ingestion.py
@@ -0,0 +1,9 @@
+
+DATA_INGESTION_DIR = "data_ingestion"
+DATA_INGESTION_DOWNLOADED_DATA_DIR = "downloaded_files"
+DATA_INGESTION_FILE_NAME = "finance_complaint"
+DATA_INGESTION_FEATURE_STORE_DIR = "feature_store"
+DATA_INGESTION_FAILED_DIR = "failed_downloaded_files"
+DATA_INGESTION_METADATA_FILE_NAME = "meta_info.yaml"
+DATA_INGESTION_MIN_START_DATE = "2022-12-01"
+DATA_INGESTION_DATA_SOURCE_URL = f"https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/?date_received_max=2023-01-01&date_received_min=2022-12-01&field=all&format=json&no_aggs=true&size=1282720&sort=created_date_desc"
